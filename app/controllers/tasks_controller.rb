817d0fa441f102c336ceb86a3c05d3aaeed4f954
mizuki.kondou <mizukikuma0513@gmail.com>
2023-01-13 17:13:41 +0000

    Lesson20-kadai1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,15 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  
   def index
-    @tasks = Task.all
+    if logged_in? 
+      @tasks = Task.all
+    else 
+      render new_sessions_url
+    end
   end
 
   def show
@@ -13,7 +21,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -56,5 +65,13 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
 
